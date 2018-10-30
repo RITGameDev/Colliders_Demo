0c5c2b9e88bff065b37eb579711ac5ccd3d10acd
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2018-10-30 16:50:50 -0400

    Add the collision and trigger checking

---
+++ Assets/Scripts/TriggerChecking.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Completed
+{
+
+
+
+    public class TriggerChecking : MonoBehaviour
+    {
+
+        /// <summary>
+        /// Happens when this obejct enters a trigger
+        /// </summary>
+        /// <param name="other">The other object</param>
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("<color=regd>On Trigger enter!</color>");    
+        }
+
+        /// <summary>
+        /// Happens when this object COLLIDES with another
+        /// </summary>
+        /// <param name="collision">The colliding object</param>
+        private void OnCollisionEnter(Collision collision)
+        {
+            Debug.Log("<color=yellow>On Collision Enter!</color>");
+        }
+
+    }
+
+
+
+}
